1f944531e2248dbd78942e9368be1f49e0196a9d
ypypyxa <axypypy@yandex.ru>
2025-07-31 19:25:46 +0300

    Изменил стрелочку "назад"

--- app/src/main/java/com/pivnoydevelopment/cafeapp/features/auth/ui/register/screen/RegisterScreen.kt
+++ app/src/main/java/com/pivnoydevelopment/cafeapp/features/auth/ui/register/screen/RegisterScreen.kt
@@ -7,8 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -23,12 +22,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.pivnoydevelopment.cafeapp.R
 import com.pivnoydevelopment.cafeapp.core.ui.components.CustomTextField
 import com.pivnoydevelopment.cafeapp.core.ui.components.DoubleLines
 import com.pivnoydevelopment.cafeapp.core.ui.theme.EspressoDepth
@@ -73,7 +74,8 @@ fun RegisterScreen(
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            modifier = Modifier.size(30.dp),
+                            painter = painterResource(id = R.drawable.ic_arrow_back),
                             contentDescription = "Назад",
                             tint = ToffieShade
                         )
